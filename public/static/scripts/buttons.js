379b7a9955a0859e26295b8f9ca6bfdf809fd9a8
SnowBallTrain <65372786+CodeSalvageON@users.noreply.github.com>
2023-03-22 17:23:39 +0000

    Finish signup page

--- public/static/scripts/buttons.js
+++ public/static/scripts/buttons.js
@@ -4,10 +4,14 @@ const signupButton = document.getElementById("signup-button");
 
 // Return from Login
 const loginBackButton = document.getElementById("back-login-sec");
+const signupBackButton = document.getElementById("back-signup-sec");
 
 // Sections listing
 const mainSec = document.getElementById("main");
 const loginSec = document.getElementById("login-sec");
+const signupSec = document.getElementById("signup-sec");
+
+// Login and Back
 
 loginButton.onclick = function () {
   loadBack("/static/images/bg/antebellum.jpeg");
@@ -18,5 +22,19 @@ loginButton.onclick = function () {
 loginBackButton.onclick = function () {
   loadBack("/static/images/bg/fall.jpeg");
   $(loginSec).fadeOut(1000);
+  $(mainSec).fadeIn(1000);
+}
+
+// Signup and Back
+
+signupButton.onclick = function () {
+  loadBack("/static/images/bg/hallways.jpeg");
+  $(mainSec).fadeOut(1000);
+  $(signupSec).fadeIn(1000);
+}
+
+signupBackButton.onclick = function () {
+  loadBack("/static/images/bg/fall.jpeg");
+  $(signupSec).fadeOut(1000);
   $(mainSec).fadeIn(1000);
 }
